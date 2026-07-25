7f1a5054bbaa02972175095c0b6b4b18c4906d69
VerevkaEA <efrosinyaverevka@gmail.com>
2026-07-26 00:31:23 +0300

    Interface by JSX

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,12 @@
+import Field from "./field"
+import Button from "./Button"
+const AddTaskForm = ()=>{
+    return(
+         <form className="todo__form">
+        <Field />
+        <Button />
+      </form>
+    )
+}
+
+export default AddTaskForm

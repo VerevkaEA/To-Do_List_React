c86b22c0307eecae1271ccea4438b2b87c2c18ec
VerevkaEA <efrosinyaverevka@gmail.com>
2026-07-27 00:30:01 +0300

    Add props

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,13 +3,20 @@ import SearchTaskForm from "./SearchtaskForm"
 import TodoInfo from "./TodoInfo"
 import TodoList from "./TodoList"
 const Todo = () => {
+const tasks=[
+    {id:'task-1',title:'Buy milk', isDone:false},
+    {id:'task-2',title:'Play with cat', isDone:true}
+]
+
     return (
         <div className="todo">
             <h1 className="todo__title">To Do List</h1>
             <AddTaskForm />
             <SearchTaskForm />
-            <TodoInfo />
-            <TodoList />
+            <TodoInfo 
+            total={tasks.length}
+            done={tasks.filter(({isDone})=>isDone).length}/>
+            <TodoList tasks={tasks}/>
 
         </div>
     )
